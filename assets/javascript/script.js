b5add69ca691026260c680e7a4651820857186c4
garrett lieb <liebg08@gmail.com>
2023-12-04 21:05:03 -0800

    java edits

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+
+fetch('https://www.loc.gov/{endpoint}/?fo=json?per_page=5', {
+  // The browser fetches the resource from the remote server without first looking in the cache.
+  // The browser will then update the cache with the downloaded resource.
+  cache: 'reload',
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
